76b3e39344bd3d83fb3abd903cb9553ac6728e98
JokermanBuilt <JokermanBuilt@JokermanB>
2012-09-10 21:24:16 -0500

    Init.

---
+++ src/personalmysqlconnection/PersonalMySqlConnection.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalmysqlconnection;
+
+import db.accessor.DBAccessor;
+import db.accessor.DB_Generic;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author JokermanBuilt
+ */
+public class PersonalMySqlConnection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        DBAccessor db;
+        
+        db = new DB_Generic();
+        
+        db.openConnection("com.mysql.jdbc.Driver", "jdbc:mysql://jokerman.doesntexist.com:3306", "Jokerman", "DoubleScreen1");
+        //db.openConnection("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306", "Jokerman", "DoubleScreen1");
+
+        String sql = "Select * From sakila.actor";
+        
+        List<Map> rawData = db.findRecords(sql, true);
+        
+        for(Map record: rawData){
+            System.out.print(record.get("actor_id").toString());
+            System.out.print(" " + record.get("first_name").toString());
+            System.out.print(" " + record.get("last_name").toString());
+            System.out.println(" " + record.get("last_update").toString());
+        }
+
+    }
+}
